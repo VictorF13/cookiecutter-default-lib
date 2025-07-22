2191a174c3d2d092766d6c211bf65c812f86cc13
VictorF13 <victorabreuf13@gmail.com>
2025-07-22 01:49:35 -0300

    Initial commit

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Exit on error
+set -e
+
+# Add development dependencies
+uv add --dev ruff
+uv add --dev ty
+uv add --dev pre-commit
+
+# Initialize git and install pre-commit hooks
+git init -b "main"
+uv run pre-commit install
+uv run pre-commit autoupdate
+uv run pre-commit install
+
+# Make initial commit
+git add .
+uv run pre-commit run --all-files
+git add .
+git commit -m "Initial commit"
+
+echo "Project {{ cookiecutter.project_name }} is ready!"
---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_slug}}/__init__.py
@@ -0,0 +1,1 @@
+"""Entry point for the {{ cookiecutter.project_name }} library."""
