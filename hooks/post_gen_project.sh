71448f40391a62ba20d061c6ef28497f0d6a6fda
VictorF13 <victorabreuf13@gmail.com>
2026-04-22 01:32:36 -0300

    fix: remove buggy initial staging

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -15,8 +15,6 @@ git init -b "main"
 uv run prek install
 uv run prek autoupdate || true
 
-git add .
 uv run prek run --all-files
-git restore --staged
 
 printf 'Project %s is ready!\n' "{{ cookiecutter.project_name }}"
